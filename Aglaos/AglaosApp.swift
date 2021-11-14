26b0abc0a45e27d5a2ccb826184ccc1ac6588658
Dylan Elliott <dylan.elliott@gmail.com>
2021-11-14 11:45:07 +1100

    Initial commit

---
+++ Aglaos/AglaosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AglaosApp.swift
+//  Aglaos
+//
+//  Created by Dylan Elliott on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AglaosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
